b1a4417a9056402b2cbd5f6d82e00b47d85dc489
Shiva Chemburkar <schemburkar@mogawaeng.com>
2024-04-29 14:08:54 -0700

    domain and theme change

--- notes/docusaurus.config.js
+++ notes/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://report.ogawa.us',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
