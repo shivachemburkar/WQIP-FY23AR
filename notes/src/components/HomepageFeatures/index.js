898f40a2891ba96f1873ca8afcdd03d546a8ee74
shiva <schemburkar@mogawaeng.com>
2024-05-10 15:05:34 -0700

    More landing page styling

--- notes/src/components/HomepageFeatures/index.js
+++ notes/src/components/HomepageFeatures/index.js
@@ -20,18 +20,14 @@ function Feature({Svg, title, description}) {
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
+    <section className={clsx(styles.features, styles.lightBackground)}>
       <div className="container">
-        <div className="row">
-        <img src={imgMap} alt="Hydrologic Areas" useMap="#image-map" />
-        <map className='image-map' name="image-map">
-          <area target="" alt="Loma Alta" title="Loma Alta" href="/docs/wqipgoals/HydrologicAreas/Loma%20Alta%20Water%20Quality%20Goals" coords="80,121,244,170" shape="rect" />
-          <area target="" alt="Buena Vista" title="Buena Vista" href="/docs/wqipgoals/HydrologicAreas/buena-vista" coords="121,211,376,182" shape="rect" />
-          <area target="" alt="Agua Hedionda" title="Agua Hedionda" href="/docs/wqipgoals/HydrologicAreas/agua-hedionda" coords="418,232,195,324" shape="rect" />
-          <area target="" alt="Encinas" title="Encinas" href="/docs/wqipgoals/HydrologicAreas/encinas" coords="149,348,270,380" shape="rect" />
-          <area target="" alt="San Marcos" title="San Marcos" href="/docs/category/san-marcos-1" coords="441,336,284,453" shape="rect" />
-          <area target="" alt="Escondido" title="Escondido" href="/docs/wqipgoals/HydrologicAreas/escondido-creek" coords="463,523,772,358" shape="rect" />
-        </map>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/Carlsbad-Watershed-Management-Area/WQIP-Report/Introduction">
+            Dive Deeper into the Data
+          </Link>
         </div>
       </div>
     </section>
--- notes/src/pages/index.js
+++ notes/src/pages/index.js
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+// import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import imgMap from '../../static/img/CWMA.png';
 
 import Heading from '@theme/Heading';
@@ -11,16 +11,14 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function RiverElement() {
   return (
-    <section className="NativeApps Section light" style={{ backgroundImage: `url(${useBaseUrl('img/river.jpg')})` }}>
-      <div className="TwoColumns reverse">
-        <div className="column first right">
-          <div>
-            <h2 className="Heading">Read up on the Water Quality health in the different hydrologic areas of the Carlsbad Watershed Management Area</h2>
-            <div dangerouslySetInnerHTML={{ __html: "Your introductory text here" }} />
-          </div>
+    <section className={clsx('NativeApps', styles.Section, styles.light)} style={{ backgroundImage: `url(${useBaseUrl('/img/river.jpg')})` }}>
+      <div className={styles.TwoColumns + ' reverse'}>
+        <div className={styles.column + ' first right'}>
+          <h2>Read up on the Water Quality health in the different hydrologic areas of the Carlsbad Watershed Management Area</h2>
+          <p>Your introductory text here</p>
         </div>
-        <div className="column last left">
-          <img alt="Phones" src={useBaseUrl('img/homepage/phones.png')} />
+        <div className={styles.column + ' last left'}>
+          <img alt="River View" src={useBaseUrl('img/homepage/river.png')} />
         </div>
       </div>
     </section>
@@ -31,33 +29,36 @@ function RiverElement() {
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', 'hero--primary', styles.heroBanner, 'HeaderHero')}>
+      {/* Container to manage the layout inside the header, not the header itself */}
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/Carlsbad-Watershed-Management-Area/WQIP-Report/Introduction">
-            Let's Get Started!
-          </Link>
+        <div className="header-content"> {/* Additional div for better control */}
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/Carlsbad-Watershed-Management-Area/WQIP-Report/Introduction">
+              Let's Get Started!
+            </Link>
+          </div>
         </div>
       </div>
     </header>
   );
 }
+
+
+
+
 // ⏱️
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`CWMA Water Quality FY21/22`}
+      title={`CWMA Water Quality FY23`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
         <RiverElement />
       </main>
     </Layout>
